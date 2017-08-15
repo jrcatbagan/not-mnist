b96c47d1c907cdeb1f6e57d009cd017837f85ae3
Jarielle Catbagan <jariellecatbagan@gmail.com>
2017-08-14 22:11:24 -0700

    Convert the Tensorflow linear model to use one-hot vectors and to use a stochastic gradient descent approach

--- tensorflow-linear.py
+++ tensorflow-linear.py
@@ -5,10 +5,13 @@ import pickle
 import os
 
 ROOT_DIRECTORY     = '.'
-IMAGE_PIXEL_WIDTH  = 28
-IMAGE_PIXEL_HEIGHT = 28
-ITERATIONS         = 1000
-BATCH_SIZE         = 100
+TRAINING_ITERATIONS = 50000
+TRAINING_BATCH_SIZE = 256
+IMAGE_PIXEL_WIDTH   = 28
+IMAGE_PIXEL_HEIGHT  = 28
+HIDDEN_NEURON_COUNT = 1024
+NUMBER_OF_CLASSES   = 10
+
 
 if __name__ == "__main__":
     pickle_file_path = os.path.join(ROOT_DIRECTORY, 'dataset.pickle')
@@ -20,53 +23,72 @@ if __name__ == "__main__":
     number_of_pixels = IMAGE_PIXEL_WIDTH * IMAGE_PIXEL_HEIGHT
 
     # Initialize the structure used for the inputs
-    X = tf.placeholder(tf.float32, [None, number_of_pixels])
+    inputs = tf.placeholder(tf.float32, [None, number_of_pixels])
     # Initialize the structure used for the weight parameters
     W = tf.Variable(tf.zeros([number_of_pixels, 10]))
     # Initialize the structure used for the bias parameters
     b = tf.Variable(tf.zeros([10]))
 
-    y = tf.nn.softmax(tf.matmul(X, W) + b)
+    outputs_prediction = tf.nn.softmax(tf.matmul(inputs, W) + b)
 
-    y_ = tf.placeholder(tf.float32, [None, 10])
+    outputs_actual = tf.placeholder(tf.float32, [None, 10])
 
-    cross_entropy = tf.reduce_mean((-1.0 * tf.reduce_sum(y_ * tf.log(y),\
+    cross_entropy = tf.reduce_mean((-1.0 * tf.reduce_sum(outputs_actual * tf.log(outputs_prediction),\
             reduction_indices=[1])))
 
-    train = tf.train.GradientDescentOptimizer(0.05).minimize(cross_entropy)
+    train_step = tf.train.GradientDescentOptimizer(0.005).minimize(cross_entropy)
 
     init = tf.global_variables_initializer()
 
     session = tf.Session()
 
-    training_set_labels = dataset['training_labels']
-
-    training_labels = np.zeros((len(training_set_labels), 10))
-
-    for training_set_labels_index in range(len(training_set_labels)):
-        one_hot = np.zeros((10), dtype=int)
-        one_hot[int(training_set_labels[training_set_labels_index])] = 1
-        training_labels[training_set_labels_index, :] = one_hot[:]
-
     session.run(init)
 
     print("training")
-    for i in range(ITERATIONS):
-        session.run(train, feed_dict={X : dataset['training_set'],\
-                y_ : training_labels})
+    batch_x = np.ndarray((TRAINING_BATCH_SIZE, IMAGE_PIXEL_WIDTH *\
+            IMAGE_PIXEL_HEIGHT))
+    batch_y = np.ndarray((TRAINING_BATCH_SIZE, NUMBER_OF_CLASSES))
 
-    cross_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    training_set_length = len(dataset['training_set'])
+    training_set_index = 0
+
+    for iteration_index in range(TRAINING_ITERATIONS):
+        if (training_set_index + TRAINING_BATCH_SIZE) < training_set_length:
+            batch_x[:,:] =\
+                    dataset['training_set'][training_set_index:\
+                    training_set_index + TRAINING_BATCH_SIZE,:]
+            batch_y[:,:] =\
+                    dataset['training_labels'][training_set_index:\
+                    training_set_index + TRAINING_BATCH_SIZE,:]
+            training_set_index += TRAINING_BATCH_SIZE
+        else:
+            n = training_set_length - training_set_index
+            batch_x[:n, :] =\
+                    dataset['training_set'][training_set_index:\
+                    training_set_length, :]
+            batch_y[:n, :] =\
+                    dataset['training_labels'][training_set_index:\
+                    training_set_length, :]
+            training_set_index = 0
+
+            batch_x[n:, :] =\
+                    dataset['training_set'][training_set_index:\
+                    TRAINING_BATCH_SIZE - n, :]
+            batch_y[n:, :] =\
+                    dataset['training_labels'][training_set_index:\
+                    TRAINING_BATCH_SIZE - n, :]
+            training_set_index += TRAINING_BATCH_SIZE - n
+
+        (_, l) = session.run([train_step, cross_entropy],\
+                feed_dict={inputs : batch_x,\
+                outputs_actual : batch_y})
+
+        print("iteration:", iteration_index, " - ", "loss:", l)
+
+    cross_prediction = tf.equal(tf.argmax(outputs_prediction, 1),\
+            tf.argmax(outputs_actual, 1))
 
     accuracy = tf.reduce_mean(tf.cast(cross_prediction, tf.float32))
 
-    test_set_labels = dataset['test_labels']
-
-    test_labels = np.zeros((len(test_set_labels), 10))
-
-    for test_set_labels_index in range(len(test_set_labels)):
-        one_hot = np.zeros((10), dtype=int)
-        one_hot[int(test_set_labels[test_set_labels_index])] = 1
-        test_labels[test_set_labels_index, :] = one_hot[:]
-
-    print(session.run(accuracy, feed_dict={X : dataset['test_set'],\
-            y_ : test_labels}))
+    print(session.run(accuracy, feed_dict={inputs : dataset['test_set'],\
+            outputs_actual : dataset['test_labels']}))
