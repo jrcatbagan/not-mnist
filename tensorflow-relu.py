f0dbd2639650b0e2a7d14ad7338fec53825fb7bd
Jarielle Catbagan <jariellecatbagan@gmail.com>
2017-08-19 00:39:39 -0700

    Clean up code and add validation summary metric

--- tensorflow-relu.py
+++ tensorflow-relu.py
@@ -1,89 +1,121 @@
 #!/usr/bin/python
+#
+# File: tensorflow-relu.py
+# Date: 2017-08-18
+
+# ------------------------------------------------------------------------------
+# Imports
+
 import tensorflow as tf
 import pickle
 import os
 import numpy as np
 import random
 
+# ------------------------------------------------------------------------------
+# Constant Parameters
+
 ROOT_DIRECTORY      = '.'
-TRAINING_ITERATIONS = 10000
+TRAINING_ITERATIONS = 5000
 TRAINING_BATCH_SIZE = 256
 IMAGE_PIXEL_WIDTH   = 28
 IMAGE_PIXEL_HEIGHT  = 28
+NUMBER_OF_PIXELS    = IMAGE_PIXEL_WIDTH * IMAGE_PIXEL_HEIGHT
 HIDDEN_NEURON_COUNT = 1024
 NUMBER_OF_CLASSES   = 10
-LEARNING_RATE       = 0.05
+LEARNING_RATE       = 0.003
+
+# ------------------------------------------------------------------------------
+# Main Logic
 
 if __name__ == "__main__":
+    # Read in the training, validation, and test set
     pickle_file_path = os.path.join(ROOT_DIRECTORY, 'dataset.pickle')
-
     pickle_file_handle = open(pickle_file_path, 'rb')
-
     dataset = pickle.load(pickle_file_handle)
 
-    number_of_pixels = IMAGE_PIXEL_WIDTH * IMAGE_PIXEL_HEIGHT
-
-    inputs = tf.placeholder(tf.float32, [None, number_of_pixels], name="X")
-
-    with tf.name_scope("input"):
-        weights_1 = tf.Variable(tf.truncated_normal([number_of_pixels,\
+    # Input Layer
+    with tf.name_scope("input_layer"):
+        inputs = tf.placeholder(tf.float32, [None, NUMBER_OF_PIXELS], name="X")
+        weights_1 = tf.Variable(tf.truncated_normal([NUMBER_OF_PIXELS,\
                 HIDDEN_NEURON_COUNT], stddev=0.1), name="W")
         bias_1 = tf.Variable(tf.truncated_normal([HIDDEN_NEURON_COUNT],\
                 stddev=0.1), name="B")
+        outputs_1 = tf.matmul(inputs, weights_1) + bias_1
 
-    outputs_1 = tf.matmul(inputs, weights_1) + bias_1
+    # Hidden Layer
+    with tf.name_scope("hidden_layer"):
+        hidden_layer = tf.nn.relu(outputs_1, name="hidden")
 
-    hidden_layer = tf.nn.relu(outputs_1)
-
-    with tf.name_scope("output"):
+    # Output Layer
+    with tf.name_scope("output_layer"):
         weights_2 = tf.Variable(tf.truncated_normal([HIDDEN_NEURON_COUNT,\
                 NUMBER_OF_CLASSES], stddev=0.1), name="W")
         bias_2 = tf.Variable(tf.truncated_normal([NUMBER_OF_CLASSES],\
                 stddev=0.1), name="B")
+        outputs_prediction = tf.nn.softmax(tf.matmul(hidden_layer, weights_2) +\
+                bias_2)
+        outputs_actual = tf.placeholder(tf.float32, [None, NUMBER_OF_CLASSES],\
+                name="labels")
 
-    outputs_prediction = tf.nn.softmax(tf.matmul(hidden_layer, weights_2) +\
-            bias_2)
+    # Metrics
+    with tf.name_scope("metrics"):
+        cross_entropy = tf.reduce_mean((-1.0 * tf.reduce_sum(outputs_actual *\
+                tf.log(outputs_prediction), reduction_indices=[1])))
+        cross_entropy_regularized = cross_entropy +\
+                (0.01 * tf.sqrt(tf.nn.l2_loss(weights_1))) +\
+                (0.01 * tf.sqrt(tf.nn.l2_loss(weights_2)))
+        cross_prediction = tf.equal(tf.argmax(outputs_prediction, 1),\
+                tf.argmax(outputs_actual, 1))
+        accuracy = tf.reduce_mean(tf.cast(cross_prediction, tf.float32),\
+                name="accuracy")
 
-    outputs_actual = tf.placeholder(tf.float32, [None, NUMBER_OF_CLASSES],\
-            name="labels")
+    # Optimizer
+    train_step = tf.train.GradientDescentOptimizer(LEARNING_RATE).\
+            minimize(cross_entropy)
 
-    cross_entropy = tf.reduce_mean((-1.0 * tf.reduce_sum(outputs_actual *\
-            tf.log(outputs_prediction), reduction_indices=[1])))
-
-    train_step =\
-            tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(cross_entropy)
-
-    init = tf.global_variables_initializer()
-
+    # Tensorflow Session
     session = tf.Session()
 
+    # Global Variable Initialization
+    init = tf.global_variables_initializer()
     session.run(init)
 
+    # Directory where all parameters/metrics will be written
     writer = tf.summary.FileWriter("./not-mnist-log")
     writer.add_graph(session.graph)
 
-    with tf.name_scope("accuracy"):
-        cross_prediction = tf.equal(tf.argmax(outputs_prediction, 1),\
-                tf.argmax(outputs_actual, 1))
+    # Write any additional parameters/metrics
+    with tf.name_scope("training"):
+        summary_training_accuracy =\
+                tf.summary.scalar("accuracy", accuracy)
+        summary_training_cross_entropy =\
+                tf.summary.scalar("cross entropy", cross_entropy)
+        summary_training = tf.summary.merge([summary_training_accuracy,\
+                summary_training_cross_entropy])
+    summary_validation_accuracy =\
+            tf.summary.scalar("validation accuracy", accuracy)
+    summary_validation_cross_entropy =\
+            tf.summary.scalar("validation cross entropy", cross_entropy)
+    summary_validation = tf.summary.merge([summary_validation_accuracy,\
+            summary_validation_cross_entropy], name="validation")
 
-        accuracy = tf.reduce_mean(tf.cast(cross_prediction, tf.float32))
-
-    batch_x = np.ndarray((TRAINING_BATCH_SIZE, IMAGE_PIXEL_WIDTH *\
-            IMAGE_PIXEL_HEIGHT))
+    # Initialize where a batch of training dataset will be stored
+    batch_x = np.ndarray((TRAINING_BATCH_SIZE, NUMBER_OF_PIXELS))
     batch_y = np.ndarray((TRAINING_BATCH_SIZE, NUMBER_OF_CLASSES))
 
     training_set_length = len(dataset['training_set'])
     training_set_index = 0
-
-    print(training_set_length)
-
     dataset_index_list = [n for n in range(training_set_length)]
-
     random.shuffle(dataset_index_list)
+
+    # Training Iterations
     for iteration_index in range(TRAINING_ITERATIONS):
+        # Determine if we need to reset the index to the training set
         if training_set_index >= TRAINING_BATCH_SIZE:
             training_set_index = 0
 
+        # Consilate the examples to be included in the next batch for training
         for batch_index in range(TRAINING_BATCH_SIZE):
             batch_x[batch_index, :] =\
                     dataset['training_set'][dataset_index_list[training_set_index],\
@@ -100,7 +132,13 @@ if __name__ == "__main__":
         if iteration_index % 100 == 0:
             print("iteration:", iteration_index, "training loss:", l,\
                     "accuracy:", a)
-
+            summary_training_run = session.run(summary_training, feed_dict=\
+                    {inputs : batch_x, outputs_actual : batch_y})
+            writer.add_summary(summary_training_run, iteration_index)
+            summary_validation_run = session.run(summary_validation, feed_dict=\
+                    {inputs : dataset['validation_set'],\
+                    outputs_actual : dataset['validation_labels']})
+            writer.add_summary(summary_validation_run, iteration_index)
 
     print(session.run(cross_prediction, feed_dict={inputs : dataset['test_set'],\
             outputs_actual : dataset['test_labels']}))
