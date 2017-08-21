1cfcb1fa417cdc4ebda98b9746fca810a6e04b04
Jarielle Catbagan <jariellecatbagan@gmail.com>
2017-08-20 20:45:56 -0700

    Update the notMNIST model with appropriate name scopes and summary outputs

--- tensorflow-linear.py
+++ tensorflow-linear.py
@@ -5,39 +5,61 @@ import pickle
 import os
 import random
 
-ROOT_DIRECTORY     = '.'
-TRAINING_ITERATIONS = 2000000
+ROOT_DIRECTORY      = '.'
+TRAINING_ITERATIONS = 5000
 TRAINING_BATCH_SIZE = 256
 IMAGE_PIXEL_WIDTH   = 28
 IMAGE_PIXEL_HEIGHT  = 28
+NUMBER_OF_PIXELS    = IMAGE_PIXEL_WIDTH * IMAGE_PIXEL_HEIGHT
 HIDDEN_NEURON_COUNT = 1024
 NUMBER_OF_CLASSES   = 10
 
 
 if __name__ == "__main__":
     pickle_file_path = os.path.join(ROOT_DIRECTORY, 'dataset.pickle')
-
     pickle_file_handle = open(pickle_file_path, 'rb')
-
     dataset = pickle.load(pickle_file_handle)
 
-    number_of_pixels = IMAGE_PIXEL_WIDTH * IMAGE_PIXEL_HEIGHT
+    # Input Layer
+    with tf.name_scope("input_layer"):
+        input_layer = tf.placeholder(tf.float32, [None, NUMBER_OF_PIXELS])
+        input_layer_reshaped = tf.reshape(input_layer, [-1, 28, 28, 1])
+        weights_input_to_output = tf.Variable(tf.truncated_normal(\
+                [NUMBER_OF_PIXELS, NUMBER_OF_CLASSES], stddev=0.1))
+        bias_input_to_output = tf.Variable(tf.truncated_normal(\
+                [NUMBER_OF_CLASSES], stddev=0.1))
 
-    # Initialize the structure used for the inputs
-    inputs = tf.placeholder(tf.float32, [None, number_of_pixels])
-    # Initialize the structure used for the weight parameters
-    W = tf.Variable(tf.truncated_normal([number_of_pixels, 10], stddev=0.1))
-    # Initialize the structure used for the bias parameters
-    b = tf.Variable(tf.zeros([10]))
+    # Output Layer
+    with tf.name_scope("output_layer"):
+        output_layer = tf.matmul(input_layer, weights_input_to_output) +\
+                bias_input_to_output
+        output_actual_layer = tf.placeholder(tf.float32,\
+                [None, NUMBER_OF_CLASSES])
 
-    outputs_prediction = tf.nn.softmax(tf.matmul(inputs, W) + b)
+        cross_entropy =\
+                tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(\
+                labels=output_actual_layer, logits=output_layer))
+        cross_prediction =\
+                tf.equal(tf.argmax(output_layer, 1),\
+                tf.argmax(output_actual_layer, 1))
+        accuracy = tf.reduce_mean(tf.cast(cross_prediction, tf.float32))
 
-    outputs_actual = tf.placeholder(tf.float32, [None, 10])
+    with tf.name_scope("training"):
+        summary_training_accuracy = tf.summary.scalar("accuracy", accuracy)
+        summary_training_cross_entropy = tf.summary.scalar("cross entropy",\
+                cross_entropy)
+        summary_training_image = tf.summary.image("input image",\
+                input_layer_reshaped)
+        summary_training = tf.summary.merge([summary_training_accuracy,\
+                summary_training_cross_entropy, summary_training_image])
+    with tf.name_scope("validation"):
+        summary_validation_accuracy = tf.summary.scalar("accuracy", accuracy)
+        summary_validation_cross_entropy = tf.summary.scalar("cross entropy",\
+                cross_entropy)
+        summary_validation = tf.summary.merge([summary_validation_accuracy,\
+                summary_validation_cross_entropy])
 
-    cross_entropy = tf.reduce_mean((-1.0 * tf.reduce_sum(outputs_actual * tf.log(outputs_prediction),\
-            reduction_indices=[1])))
-
-    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+    train_step = tf.train.GradientDescentOptimizer(0.05).minimize(cross_entropy)
 
     init = tf.global_variables_initializer()
 
@@ -46,17 +68,6 @@ if __name__ == "__main__":
     writer = tf.summary.FileWriter("./log-tensorflow-linear", session.graph)
 
     session.run(init)
-
-    cross_entropy_summary = tf.summary.scalar("cross entropy",\
-            cross_entropy)
-
-    cross_prediction = tf.equal(tf.argmax(outputs_prediction, 1),\
-            tf.argmax(outputs_actual, 1))
-    accuracy = tf.reduce_mean(tf.cast(cross_prediction, tf.float32))
-
-    accuracy_summary = tf.summary.scalar("accuracy", accuracy)
-
-    merged_summaries = tf.summary.merge_all()
 
     print("training")
     batch_x = np.ndarray((TRAINING_BATCH_SIZE, IMAGE_PIXEL_WIDTH *\
@@ -68,6 +79,9 @@ if __name__ == "__main__":
 
     dataset_index_list = [n for n in range(training_set_length)]
 
+    random.shuffle(dataset_index_list)
+    random.shuffle(dataset_index_list)
+    random.shuffle(dataset_index_list)
     random.shuffle(dataset_index_list)
     for iteration_index in range(TRAINING_ITERATIONS):
         if training_set_index >= TRAINING_BATCH_SIZE:
@@ -82,27 +96,17 @@ if __name__ == "__main__":
                     :]
             training_set_index += 1
 
-        (_, l) = session.run([train_step, cross_entropy],\
-                feed_dict={inputs : batch_x,\
-                outputs_actual : batch_y})
+        session.run([train_step], feed_dict={input_layer : batch_x,\
+                output_actual_layer : batch_y})
 
         if iteration_index % 100 == 0:
-            print("iteration:", iteration_index, " - ", "loss:", l)
-            s = session.run(merged_summaries, feed_dict=\
-                    {inputs : batch_x, outputs_actual : batch_y})
-            writer.add_summary(s, iteration_index)
+            summary_training_run = session.run(summary_training,\
+                    feed_dict={input_layer : batch_x,\
+                    output_actual_layer : batch_y})
+            summary_validation_run = session.run(summary_validation,\
+                    feed_dict={input_layer : dataset['validation_set'],\
+                    output_actual_layer : dataset['validation_labels']})
+            writer.add_summary(summary_training_run, iteration_index)
+            writer.add_summary(summary_validation_run, iteration_index)
 
-    cross_prediction = tf.equal(tf.argmax(outputs_prediction, 1),\
-            tf.argmax(outputs_actual, 1))
-
-    accuracy = tf.reduce_mean(tf.cast(cross_prediction, tf.float32))
-
-    print("training accuracy:", session.run(accuracy,\
-            feed_dict={inputs : dataset['training_set'],\
-            outputs_actual : dataset['training_labels']}))
-    print("validation accuracy:", session.run(accuracy,\
-            feed_dict={inputs : dataset['validation_set'],\
-            outputs_actual : dataset['validation_labels']}))
-    print("test accuracy:", session.run(accuracy,\
-            feed_dict={inputs : dataset['test_set'],\
-            outputs_actual : dataset['test_labels']}))
+    print("training complete")
